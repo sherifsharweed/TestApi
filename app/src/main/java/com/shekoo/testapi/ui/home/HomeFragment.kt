a485e710da256930fc685d342688ebc5ae040ff4
Sherif Sharweed <96727431+sherifsharweed@users.noreply.github.com>
2022-05-31 04:42:09 +0200

    get

--- app/src/main/java/com/shekoo/testapi/ui/home/HomeFragment.kt
+++ app/src/main/java/com/shekoo/testapi/ui/home/HomeFragment.kt
@@ -4,39 +4,41 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.shekoo.testapi.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentHomeBinding
+    private val homeViewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+    override fun onResume() {
+        super.onResume()
+        binding.apply {
+            button.setOnClickListener {
+                if(urlText.text.isNotEmpty()){
+                    Thread {
+                        homeViewModel.getMethod(urlText.text.toString())
+                    }.start()
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+                }
+            }
         }
-        return root
+        homeViewModel.getResponse.observe(viewLifecycleOwner, Observer {
+            binding.getResponseTextView.text = it
+        })
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+
 }
--- app/src/main/java/com/shekoo/testapi/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/shekoo/testapi/ui/home/HomeViewModel.kt
@@ -1,13 +1,61 @@
 package com.shekoo.testapi.ui.home
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonParser
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.URL
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private var _getResponse : MutableLiveData<String> = MutableLiveData()
+    var getResponse : LiveData<String> = _getResponse
+
+    fun getMethod(url : String) {
+
+        val httpURLConnection = makeHttpUrlConnection(url)
+        val responseCode = httpURLConnection.responseCode
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            _getResponse.postValue(collectResponse(httpURLConnection))
+            getResponse = _getResponse
+        } else {
+            Log.i("TAG", responseCode.toString())
+        }
+        httpURLConnection.disconnect()
     }
-    val text: LiveData<String> = _text
+
+    private fun makeHttpUrlConnection(url: String) : HttpURLConnection {
+        val httpURLConnection = URL(url).openConnection() as HttpURLConnection
+        /*httpURLConnection.setRequestProperty(
+            "Accept",
+            "application/json"
+        )*/
+        httpURLConnection.requestMethod = "GET"
+        httpURLConnection.doInput = true
+        httpURLConnection.doOutput = false
+        return httpURLConnection
+    }
+
+    private fun collectResponse(httpURLConnection: HttpURLConnection) : String {
+        val result : StringBuilder = StringBuilder()
+        val response = httpURLConnection.inputStream.bufferedReader()
+            .use { it.readText() }
+        // Get headers in response
+        val map: Map<String, List<String>> = httpURLConnection.headerFields
+        for ((key, value) in map) {
+            result.append("\n")
+            result.append(key).append(value)
+        }
+        // Convert raw JSON to pretty JSON using GSON library
+        val gson = GsonBuilder().setPrettyPrinting().create()
+        val prettyJson = gson.toJson(JsonParser.parseString(response))
+        result.append(prettyJson)
+        return result.toString()
+    }
+
+
 }
